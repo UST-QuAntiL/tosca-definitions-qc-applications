efdbb37da89bfd0377da15c206b54f9dd7b350ab
Marvin Bechtold <marvin.bechtold.dev@gmail.com>
2021-09-27 17:51:44 +0200

    Add IBMQ node and connect it to the PluginRunner
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fquantum%2Fapplications%2Fartifacttemplates/QHAna-PluginRunner-connectTo_DB-w1-wip1/files/connectTo.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fquantum%2Fapplications%2Fartifacttemplates/QHAna-PluginRunner-connectTo_DB-w1-wip1/files/connectTo.sh
@@ -5,8 +5,10 @@ cat << EOF > connect_db.sh
 export SQLALCHEMY_DATABASE_URI="mariadb+pymysql://${TARGET_DBUser}:${TARGET_DBPassword}@${TARGET_ContainerIP}:${TARGET_DBMSPort}/${TARGET_DBName}?charset=utf8mb4"
 EOF
 
-if [ -f connect_redis.sh ]; then
-    source connect_redis.sh
-    source connect_db.sh
-    source start_script.sh
+if [ -f connect_ibmq.sh ]; then
+    if [ -f connect_redis.sh ]; then
+        source connect_redis.sh
+        source connect_db.sh
+        source connect_ibmq.sh
+        source start_script.sh
 fi
